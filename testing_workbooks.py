d6e99af371fd5c8f260cbaff06a7f8b9c2f5d9d3
sayakoftheleaf <sayakchatterjee1309@gmail.com>
2018-11-08 17:29:17 +0530

    created all the headers

--- testing_workbooks.py
+++ testing_workbooks.py
@@ -1,83 +1,78 @@
 import toml
-
-from openpyxl import Workbook, load_workbook
-from openpyxl.utils import get_column_letter, column_index_from_string
+import openpyxl
 
 # translate config to dictionary
 configDict = toml.load('config.toml')
 
-# geeting the worksheet to transform
-sourceWorkbook = 
-  load_workbook('./Spreadsheets' + configDict.get('filename'), read_only=True)
-sourceSheet = sourceWorkbook.get_sheet_by_name(configDict.get(sheet.name))
+# getting the worksheet to transform
+sourceWorkbook = openpyxl.load_workbook('./Spreadsheets/' + configDict.get('filename'))
 
+# declaring source information variables
 sourceConfig = configDict.get('sheet')
 sourceRow = 0
 sourceCol = 0
+sourceSheet = 'Sheet 1' # default Sheet name in Excel
 
-for key, value in sheet.items():
+# loading up the values of the source sheet from the config
+for key, value in sourceConfig.items():
   if (str(key) == 'rows'):
     sourceRow = value
-  elif (str(key) = 'cols'):
-    sourceCol = value
+  elif (str(key) == 'cols'):
+    sourceCol = openpyxl.utils.column_index_from_string(str(value))
+  elif (str(key) == 'name'):
+    sourceSheet = sourceWorkbook.get_sheet_by_name(str(value))
 
 separator = configDict.get('separator')
 
 # output excel destination
 dest = "output_book.xlsx"
-wb = Workbook()
+wb = openpyxl.Workbook()
 outputSheet = wb.create_sheet(title="output_sheet")
 
 headers = configDict.get('header')
 
 # placing all the headers
 for key, value in headers.items():
-  columnIndex = column_index_from_string(str(key))
+  columnIndex = openpyxl.utils.column_index_from_string(str(key))
   outputSheet.cell(row=1, column=columnIndex, value=str(value))
 
 # iterating through the source sheet
 for iterRow in range(1, sourceRow + 1):
-
   outputDict = {} # empty dictionary for every row
+  
   for iterCol in range(1, sourceCol + 1):
 
-    content = sourceSheet.cell(row = iterRow, column = iterCol)
-    colLetter = get_column_letter(iterCol)
+    content = sourceSheet.cell(row = iterRow, column = iterCol).internal_value
+    if (str(content) == "None"):
+      content = "N/A"
+    colLetter = openpyxl.utils.get_column_letter(iterCol)
         
     # check if the present column maps to something in the config file
     # default returnvalue is NoMap
-    mapsTo = configDict.get(colLetter, 'NoMap')
-        
-    # If source doesn't map to anything, ignore it and move on
-    if (mapsTo == 'NoMap'):
-      continue
-    else:
+    mapsTo = configDict.get('mapto')
 
-      # if the output column doesn't yet exist in the output hash table
-      # create it
-      # otherwise append to the existing array at for the map
-      if (outputDict.get(mapsTo, 'None') == None):
-        # creating a new array in case nothing exists
-        outputDict[mapsTo] = [content]
+    if colLetter in mapsTo:
+      # if a mapping exists in config, put the value
+      # in the output dictionary
+      outputCol = mapsTo[colLetter]
+      if outputCol in outputDict:
+        outputDict[outputCol].append(content)
       else:
-        # appending to original array otherwise
-        outputDict[mapsTo].append(content)
+        outputDict[outputCol] = [content]
 
   for key, dictValue in outputDict.items():
+    # adjusting to fit the headers
     normalizedRow = iterRow + 2
-    columnIndex = column_index_from_string(key)
+    columnIndex = openpyxl.utils.column_index_from_string(key)
 
     # converting the contents of the cell into a string
     # separated by the specified separator
-    formattedValue = separator.join(map(str, dictValue))
+    if (len(dictValue) > 1):
+      formattedValue = separator.join(map(str, dictValue))
+    else:
+      formattedValue = str(dictValue[0])
+    
     # inserting cells into the new
-    outputSheet.cell(row = normalizedRow, column = columnIndex, value = dictValue)
+    outputSheet.cell(row = normalizedRow, column = columnIndex, value = formattedValue)
 
-
-
-
-# TODO: transform the dictionary to a spreadsheet
-
-# All the values in the cols are in sequential ascending order
-# When transforming them to spreadsheets, just pasting them in 
-# successive rows
+wb.save(dest)
