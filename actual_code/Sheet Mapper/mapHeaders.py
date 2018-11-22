7add56c7053aec40f94060d6af3a62df45ee531a
sayakoftheleaf <sayakchatterjee1309@gmail.com>
2018-11-22 18:01:17 +0530

    added .gitignore

--- actual_code/Sheet Mapper/mapHeaders.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-
-def mapHeaders (configDict, outputSheet):
-  # placing all the headers
-  for key, value in configDict.get('header').items():
-    columnIndex = openpyxl.utils.column_index_from_string(str(key))
-    outputSheet.cell(row=1, column=columnIndex, value=str(value))
-
-  return outputSheet
